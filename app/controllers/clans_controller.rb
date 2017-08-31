b5d6aef792aee487243a41441a0e7d1f841c32de
DennisKimani <dennisngugi.dk@gmail.com>
2017-08-31 11:37:23 +0300

    called the clan model.

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -1,2 +1,50 @@
 class ClansController < ApplicationController
+  def index
+    @clans = Clan.all
+    render :index
+  end
+  def show
+    @clan = Clan.find(params[:id])
+    render :show
+  end
+
+  def new
+    @clan = Clan.new
+    render :new
+  end
+
+  def create
+    @clan = Clan.new(clan_params)
+    if @clan.save
+      redirect_to clans_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @clan = Clan.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @clan = Clan.find(params[:id])
+    if @clan.update(clan_params)
+      redirect_to clans_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @clan = Clan.find(params[:id])
+    @clan.destroy
+    redirect_to clans_path
+  end
+
+  private
+
+  def clan_params
+    params.require(:clan).permit(:name, :quote)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :clans
   root :to => 'homes#index'
 end
